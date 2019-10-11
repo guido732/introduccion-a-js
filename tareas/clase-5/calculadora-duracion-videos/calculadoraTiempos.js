29c416108a8a717a05011267dc154a7f6204fa49
guido732 <guido732@gmail.com>
2019-10-11 14:08:51 -0300

    ctrl z

--- tareas/clase-5/calculadora-duracion-videos/calculadoraTiempos.js
+++ tareas/clase-5/calculadora-duracion-videos/calculadoraTiempos.js
@@ -97,29 +97,24 @@ function limpiarInputs() {
 	document.querySelector("#cantidadClasesTotal").value = "";
 }
 function deshabilitarInputsEtapa1() {
-	// document.querySelector("#empezar").disabled = true;
-	// document.querySelector("#cantidadClasesTotal").disabled = true;
-	document.querySelector(".inputs-etapa-1").hidden = true;
+	document.querySelector("#empezar").disabled = true;
+	document.querySelector("#cantidadClasesTotal").disabled = true;
 }
 function deshabilitarInputsEtapa2() {
-	// document.querySelector("#duracionHoras").disabled = true;
-	// document.querySelector("#duracionMinutos").disabled = true;
-	// document.querySelector("#duracionSegundos").disabled = true;
-	// document.querySelector("#proximaClase").disabled = true;
-
-	document.querySelector(".inputs-etapa-2").hidden = true;
+	document.querySelector("#duracionHoras").disabled = true;
+	document.querySelector("#duracionMinutos").disabled = true;
+	document.querySelector("#duracionSegundos").disabled = true;
+	document.querySelector("#proximaClase").disabled = true;
 }
 function habilitarInputsEtapa1() {
-	// document.querySelector("#empezar").disabled = false;
-	// document.querySelector("#cantidadClasesTotal").disabled = false;
-	document.querySelector(".inputs-etapa-1").hidden = false;
+	document.querySelector("#empezar").disabled = false;
+	document.querySelector("#cantidadClasesTotal").disabled = false;
 }
 function habilitarInputsEtapa2() {
-	// document.querySelector("#duracionHoras").disabled = false;
-	// document.querySelector("#duracionMinutos").disabled = false;
-	// document.querySelector("#duracionSegundos").disabled = false;
-	// document.querySelector("#proximaClase").disabled = false;
-	document.querySelector(".inputs-etapa-2").hidden = false;
+	document.querySelector("#duracionHoras").disabled = false;
+	document.querySelector("#duracionMinutos").disabled = false;
+	document.querySelector("#duracionSegundos").disabled = false;
+	document.querySelector("#proximaClase").disabled = false;
 }
 function reiniciarValoresCounters() {
 	horasTotales = 0;
