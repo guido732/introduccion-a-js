92962aff7a29c93f2bcbddc155daec6a4a7ae180
guido732 <guido732@gmail.com>
2019-10-11 18:52:31 -0300

    Función obtener Promedio

--- tareas/clase-5/calculadoraListaOrdenada/main.js
+++ tareas/clase-5/calculadoraListaOrdenada/main.js
@@ -5,3 +5,17 @@
 // 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+const listaOriginal = document.querySelectorAll("li");
+
+function obtenerPromedio(array) {
+	let counter = 0;
+	let promedio = 0;
+	for (let elemento of array) {
+		counter += Number(elemento.innerHTML);
+	}
+	promedio = counter / array.length;
+	return promedio;
+}
+
+console.log(obtenerPromedio(listaOriginal));
