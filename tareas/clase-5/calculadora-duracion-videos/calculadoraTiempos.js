58136a95dd43a0b232d446f8bb73f92f510cb550
guido732 <guido732@gmail.com>
2019-10-11 11:00:06 -0300

    creado de función inical y habilitar/deshabilitar botones
    Generación de primera función que al hacer clic en el botón iniciar deshabilita el primer input y habilita los siguientes para completar los datos de las clases

--- tareas/clase-5/calculadora-duracion-videos/calculadoraTiempos.js
+++ tareas/clase-5/calculadora-duracion-videos/calculadoraTiempos.js
@@ -5,3 +5,31 @@
 // cada dato.
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
+
+/* 
+1- ingresar clases totales a calcular
+2- hacer clic en empezar
+3- deshabilitar input + botón
+4- habilitar otros inputs + botón de próxima clase + claseCounter++
+5- completar + hacer clic en proxima clase
+6- alojar valores, limpiar formularios y repetir
+7- al llegar al valor de clases final salir del loop iterativo
+8- deshabilitar todos los inputs
+9- hacer cálculos y mostrar valores en <strong>
+10- botón de empezar de nuevo
+11- reiniciar (limpiar, inhabilitar y habilitar valores correspondientes)
+*/
+
+document.querySelector("#empezar").onclick = function(e) {
+	e.preventDefault();
+	let cantidadClases = document.querySelector("#cantidadClases").value;
+	if (cantidadClases === "") {
+		cantidadClases = 0;
+	}
+	document.querySelector("#empezar").disabled = true;
+	document.querySelector("#cantidadClases").disabled = true;
+	document.querySelector("#duracionHoras").disabled = false;
+	document.querySelector("#duracionMinutos").disabled = false;
+	document.querySelector("#duracionSegundos").disabled = false;
+	document.querySelector("#proximaClase").disabled = false;
+};
