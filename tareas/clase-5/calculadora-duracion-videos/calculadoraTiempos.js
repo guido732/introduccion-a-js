b68b861922d74ca12888349369abdff5858690b8
guido732 <guido732@gmail.com>
2019-10-11 12:04:13 -0300

    funcionamiento principal
    Funcionamiento principal agregado, separando en funciones cortas y emprolijando

--- tareas/clase-5/calculadora-duracion-videos/calculadoraTiempos.js
+++ tareas/clase-5/calculadora-duracion-videos/calculadoraTiempos.js
@@ -20,16 +20,75 @@
 11- reiniciar (limpiar, inhabilitar y habilitar valores correspondientes)
 */
 
+// Forzar disable para cuando se hace F5 o CTRL+R en navegador
+deshabilitarInputsEtapa2();
+limpiarInputs();
+document.querySelector("#cantidadClasesTotal").value = "";
+
+let horasTotales = 0;
+let minutosTotales = 0;
+let segundosTotales = 0;
+
 document.querySelector("#empezar").onclick = function(e) {
+	// Evitar que el boton empezar envíe el form
 	e.preventDefault();
-	let cantidadClases = document.querySelector("#cantidadClases").value;
+
+	// Tomar el valor del input y guardarlo en cantidad clases
+	let cantidadClases = document.querySelector("#cantidadClasesTotal").value;
+
+	// Si se deja el input vacío se lo toma por default como 0;
 	if (cantidadClases === "") {
 		cantidadClases = 0;
 	}
+	// Deshabilitación de input + boton de la 1ra etapa
 	document.querySelector("#empezar").disabled = true;
-	document.querySelector("#cantidadClases").disabled = true;
+	document.querySelector("#cantidadClasesTotal").disabled = true;
+
+	//  No me funciona ésto para no tener que escribir las 4 líneas de abajo
+	// document.querySelectorAll(".input-clases").disabled = false;
+
+	// Habilitación de inputs correspondientes para la 2da etapa
 	document.querySelector("#duracionHoras").disabled = false;
 	document.querySelector("#duracionMinutos").disabled = false;
 	document.querySelector("#duracionSegundos").disabled = false;
 	document.querySelector("#proximaClase").disabled = false;
+
+	// Inicialización del counter de clases
+	let claseCounter = 1;
+	document.querySelector("#claseCounter").innerHTML = `Clase ${claseCounter} de ${cantidadClases}`;
+
+	if (claseCounter <= cantidadClases) {
+		document.querySelector("#proximaClase").onclick = function(e) {
+			e.preventDefault();
+			sumarValoresInputs();
+			limpiarInputs();
+		};
+		claseCounter++;
+		document.querySelector("#claseCounter").innerHTML = `Clase ${claseCounter} de ${cantidadClases}`;
+	} else {
+		document.querySelector("#proximaClase").onclick = function(e) {
+			e.preventDefault();
+			document.querySelector(
+				"#result"
+			).innerHTML = `${horasTotales} horas, ${minutosTotales} minutos y ${segundosTotales} segundos`;
+		};
+	}
 };
+
+function sumarValoresInputs() {
+	horasTotales += Number(document.querySelector("#duracionHoras").value);
+	minutosTotales += Number(document.querySelector("#duracionMinutos").value);
+	segundosTotales += Number(document.querySelector("#duracionSegundos").value);
+}
+
+function limpiarInputs() {
+	document.querySelector("#duracionHoras").value = "";
+	document.querySelector("#duracionMinutos").value = "";
+	document.querySelector("#duracionSegundos").value = "";
+}
+function deshabilitarInputsEtapa2() {
+	document.querySelector("#duracionHoras").disabled = true;
+	document.querySelector("#duracionMinutos").disabled = true;
+	document.querySelector("#duracionSegundos").disabled = true;
+	document.querySelector("#proximaClase").disabled = true;
+}
